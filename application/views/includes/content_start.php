e83bcea54a56e1801f5b0476e4220c61b687463b
David Collins <davidcollins4481@gmail.com>
2012-08-09 18:31:16 -0400

    just an incremental commit

---
+++ application/views/includes/content_start.php
@@ -0,0 +1,22 @@
+  <div id="main">
+    <header><div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="index.html">CSS3<span class="logo_colour">_photo_two</span></a></h1>
+          <h2>Simple. Contemporary. Website Template.</h2>
+        </div>
+      </div>
+      <!-- include nav here -->
+      
+      <!-- /include nav here -->
+    </header>
+    <div id="site_content">
+      <div class="gallery">
+        <ul class="images">
+<li class="show"><img width="950" height="300" src="images/1.jpg" alt="photo_one"></li>
+          <li><img width="950" height="300" src="images/2.jpg" alt="photo_two"></li>
+          <li><img width="950" height="300" src="images/3.jpg" alt="photo_three"></li>
+        </ul>
+</div>
+      <div id="sidebar_container">
+        <div class="sidebar">
---
+++ application/views/includes/html_start.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>CSS3_photo_two</title>
+<meta name="description" content="website description">
+<meta name="keywords" content="website keywords, website keywords">
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<!-- modernizr enables HTML5 elements and feature detects --><script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
+</head>
