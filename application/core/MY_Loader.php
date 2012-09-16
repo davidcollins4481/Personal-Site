7c841b16c76d009d7a03b86422119055f17d096d
David Collins <davidcollins4481@gmail.com>
2012-09-16 15:54:47 -0400

    clean up boiler plate page

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -13,18 +13,18 @@ class MY_Loader extends CI_Loader {
     function view($view, $vars = array(), $return = FALSE) {
         $agent = load_class('User_agent');
         $mobile = $agent->is_mobile;
-    
+
         // for testing
         if (isset($_COOKIE["mobile"])) {
             $mobile = $_COOKIE["mobile"] == 1 ? true : false;
         }
-        
+
         if ($mobile) {
             $view = 'mobile/'.$view;
         } else {
             $view = 'pc/'.$view;
         }
-        
+
         return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
     }
 }
--- application/views/pc/global/content_end.php
+++ application/views/pc/global/content_end.php
@@ -1,33 +1,20 @@
- </div>
-        <div class="sidebar">
-          <h3>Useful Links</h3>
-          <ul>
-<li><a href="#">First Link</a></li>
-            <li><a href="#">Another Link</a></li>
-            <li><a href="#">And Another</a></li>
-            <li><a href="#">Last One</a></li>
-          </ul>
-</div>
-      </div>
-      <div class="content">
-        <h1>Another Page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.</p>
-      </div>
+        </div>
+        <footer>
+            <p>Copyright &copy; CSS3_photo_two | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a></p>
+        </footer>
     </div>
-    <footer><p>Copyright &copy; CSS3_photo_two | <a href="http://www.css3templates.co.uk">design from css3templates.co.uk</a>
-</p>
-    </footer>
-</div>
-  <p>&nbsp;</p>
-  <!-- javascript at the bottom for fast page loading -->
-  <script type="text/javascript" src="js/jquery.js"></script><script type="text/javascript" src="js/jquery.easing-sooper.js"></script><script type="text/javascript" src="js/jquery.sooperfish.js"></script><script type="text/javascript" src="js/image_fade.js"></script><script type="text/javascript">
-    $(document).ready(function() {
-      $('#images').kwicks({
-        max : 600,
-        spacing : 2
-      });
-      $('ul.sf-menu').sooperfish();
-    });
-  </script>
+    <p>&nbsp;</p>
+    <!-- javascript at the bottom for fast page loading -->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.easing-sooper.js"></script>
+    <script type="text/javascript" src="js/jquery.sooperfish.js"></script>
+    <script type="text/javascript" src="js/image_fade.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+          $('#images').kwicks({
+            max : 600,
+            spacing : 2
+          });
+          $('ul.sf-menu').sooperfish();
+        });
+    </script>
