612ab0fd496fc8316aaf6115c5356817f38068c6
David Collins <davidcollins4481@gmail.com>
2013-02-09 14:20:29 -0500

    little bit of copy for gears page

--- application/views/pc/static/gears.php
+++ application/views/pc/static/gears.php
@@ -4,7 +4,12 @@
 
     <div class="content">
         <h1>CSS Gears</h1>
-        <p>All animations are pure CSS (This will not work on IE or Opera)</p>
+        <p>
+            All animations are pure CSS (This will not work on IE or Opera). 
+            There is a lot of fun to be had with these...especially when they're
+            better supported. Here's where all of the heavy lifting is done if you're
+            curious <a href="/css/projects/gears.css">here</a>.
+        </p>
         <div id="gear-container">
             <div id="gear1" class="gear"></div>
             <div id="gear2" class="gear"></div>
