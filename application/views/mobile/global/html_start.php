03bbe92e938d8a900ac2892a0c90ce9c6fc05df0
David Collins <davidcollins4481@gmail.com>
2013-02-09 00:17:57 -0500

    mobile css changes

--- application/views/mobile/global/html_start.php
+++ application/views/mobile/global/html_start.php
@@ -2,9 +2,8 @@
 <html>
 <head>
     <title>FranticPedantic</title>
-    <meta name="description" content="website description">
-    <meta name="keywords" content="website keywords, website keywords">
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="/css/mobile/style.css">
+
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="/css/mobile/style.css" media="all">
     <script type="text/javascript" src="/js/modernizr-1.5.min.js"></script>
 </head>
