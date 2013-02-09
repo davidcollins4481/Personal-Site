6e9bf8220e6baa62f45b8e852ed3454934d17c8b
David Collins <davidcollins4481@gmail.com>
2013-02-09 09:54:39 -0500

    messing around with mobile nav

--- application/views/mobile/global/nav.php
+++ application/views/mobile/global/nav.php
@@ -1,5 +1,5 @@
 <nav>
-    <ul class="sf-menu" id="nav">
+    <ul class="sf-menu" id="nav" data-role="listview" data-inset="true" data-filter="true">
         <li><a href="/">Home</a></li>
         <li><a href="/main/playground">Playground</a></li>
         <li><a href="/main/contact">Contact Me</a></li>
