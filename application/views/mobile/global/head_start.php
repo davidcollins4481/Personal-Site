db0873e67c4ccc089e3efd92893a932b069a7dd1
David Collins <davidcollins4481@gmail.com>
2013-03-07 22:38:53 -0500

    view changes for mobile section

---
+++ application/views/mobile/global/head_start.php
@@ -0,0 +1,15 @@
+<head>
+    <title>FranticPedantic</title>
+    <link rel="stylesheet" href="/themes/frantic-pedantic-mobile-theme.min.css" />
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile.structure-1.3.0-rc.1.min.css" />
+    <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile-1.3.0-rc.1.min.js"></script>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="/css/mobile/style.css" media="all">
+    <script type="text/javascript" src="/js/modernizr-1.5.min.js"></script>
+    <script type="text/javascript" src="/js/sprintf-0.6.js"></script>
+
+    <!-- projects -->
+    <script src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
+    <script type="text/javascript" src="/js/projects/geolocation.js"></script>
+
