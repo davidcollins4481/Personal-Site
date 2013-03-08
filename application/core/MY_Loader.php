3332a8151ff2210f2910bbc9acd79ebad2399da8
David Collins <davidcollins4481@gmail.com>
2013-03-07 22:03:46 -0500

    merge my mobile loader changes with sparks

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -261,5 +261,25 @@ class MY_Loader extends CI_Loader
                 $this->model($autoload['model']);
             }
         }
-	}
+    }
+
+    /* MY MOBILE CHANGES */
+    function view($view, $vars = array(), $return = FALSE) {
+        $agent = load_class('User_agent');
+        $mobile = $agent->is_mobile;
+
+        // for testing
+        if (isset($_COOKIE["mobile"])) {
+            $mobile = $_COOKIE["mobile"] == 1 ? true : false;
+        }
+
+        if ($mobile) {
+            $view = 'mobile/'.$view;
+        } else {
+            $view = 'pc/'.$view;
+        }
+
+        return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => $this->_ci_object_to_array($vars), '_ci_return' => $return));
+    }
+
 }
