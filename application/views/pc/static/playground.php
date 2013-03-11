3b0f114614481e79f6ae6d31c6382ad72757d67d
David Collins <davidcollins4481@gmail.com>
2013-03-11 08:17:20 -0400

    add mustache to playground page

--- application/views/pc/static/playground.php
+++ application/views/pc/static/playground.php
@@ -1,11 +1,21 @@
 <?php $this->load->view("global/html_start.php");?>
 <?php $this->load->view("global/head_start.php");?>
+
+<style type="text/css">
+    p {
+        padding: 10px 0 0 0;
+    }
+</style>
+
 <?php $this->load->view("global/head_end.php");?>
 <?php $this->load->view("global/content_start.php");?>
 
     <div class="content">
         <p>Things I tinker with for fun when I get some extra time</p>
         <a href="/main/gears">CSS gears</a>
+
+        <p>Playing with the Mustache templating </p>
+        <a href="/main/mustache">Mustache fun</a>
     </div>
 
 <?php $this->load->view("global/content_end.php");?>
