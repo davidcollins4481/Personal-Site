6ef61e63017a277486d84a94370ec78be8a44b06
David Collins <davidcollins4481@gmail.com>
2013-03-11 09:02:14 -0400

    polish the layout a bit...just playing around

--- application/views/pc/static/mustache.php
+++ application/views/pc/static/mustache.php
@@ -1,6 +1,39 @@
 <?php $this->load->view("global/html_start.php");?>
 <?php $this->load->view("global/head_start.php");?>
 
+<style type="text/css">
+    #attach_point {
+        float: left;
+        width: 100%;
+    }
+
+    .person {
+        float: left;
+        width: 50%;
+    }
+
+        .person h3 {
+            padding: 0;
+        }
+
+        .person ul {
+
+        }
+
+        .person li {
+            background: none !important;
+            padding: 0 !important;
+        }
+
+        .party-D .party-label {
+            color: blue;
+        }
+
+        .party-R .party-label {
+            color: red;
+        }
+</style>
+
 <script src="http://github.com/janl/mustache.js/raw/master/mustache.js" type="text/javascript"></script>
 
 <script type="text/javascript">
@@ -8,6 +41,12 @@
     $(document).ready(function() {
         var obj = <?php echo $house_members ?>;
         var data = obj.results[0];
+
+        // a little bit of sorting
+        data.members.sort(function(a, b) {
+            return parseInt(a.district) > parseInt(b.district) ? 1 : -1;
+        });
+
         var template = $('#congress_person').html();
         var rendered = Mustache.to_html(template, data);
         $('#attach_point').html(rendered);
@@ -17,11 +56,11 @@
 
 <script id="congress_person" type="text/template">
     {{#members}}
-    <div style="float:left;margin: 10px;">
+    <div class="person party-{{party}}">
         <h3>{{first_name}} {{last_name}}</h3>
         <ul>
             <li>District: {{district}}</li>
-            <li>Party Affiliation: {{party}}</li>
+            <li class="party-label">Party Affiliation: {{party}}</li>
             <li>Votes with Party: {{votes_with_party_pct}}</li>
         </ul>
     </div>
@@ -34,7 +73,7 @@
     <div class="content">
         <h1>Mustache Fun</h1>
         <p>
-            <a href="">Mustache</a> is a cool templating library that I've been meaning to check out. Let's do something
+            <a href="http://mustache.github.com/">Mustache</a> is a cool templating library that I've been meaning to check out. Let's do something
             simple like get a list of all of Ohio's US House of Representative members from the NY Time API.
         </p>
         <div id="attach_point"></div>
