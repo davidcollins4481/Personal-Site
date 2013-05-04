231b82d6076d4c8e6f021d3942c91d6d7197cd4e
David Collins <davidcollins4481@gmail.com>
2013-05-04 11:28:17 -0400

    minor text change

--- application/views/pc/static/gears.php
+++ application/views/pc/static/gears.php
@@ -9,17 +9,16 @@
     <div class="content">
         <h1>CSS Gears</h1>
         <p>
-            All animations are pure CSS (This will not work on IE or Opera). 
+            All animations are pure CSS (This will not work on IE or Opera).
             There is a lot of fun to be had with these...especially when they're
-            better supported. Here's where all of the heavy lifting is done if you're
-            curious <a href="/css/projects/gears.css">here</a>.
+            better supported. <a href="/css/projects/gears.css">Here</a> is where
+            all of the heavy lifting is done if you're curious.
         </p>
         <div id="gear-container">
             <div id="gear1" class="gear"></div>
             <div id="gear2" class="gear"></div>
             <div id="gear3" class="gear"></div>
         </div>
-
     </div>
 
 <?php $this->load->view("global/content_end.php");?>
