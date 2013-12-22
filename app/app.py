3ee1d3de614b13566c244394ac7b028b53292750
David Collins <davidcollins4481@gmail.com>
2013-12-21 22:01:36 -0500

    move bootstrap around

--- app/app.py
+++ app/app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('hello.html')
-
-@app.route("/test")
-def test():
-    return "new page sadsd"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     if os.environ.get('DEBUG'):
