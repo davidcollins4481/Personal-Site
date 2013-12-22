ca3389af2d2d56da7d828c781e53c7b596885193
David Collins <davidcollins4481@gmail.com>
2013-12-22 12:20:17 -0500

    save point

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('base.html')
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     if os.environ.get('DEBUG'):
